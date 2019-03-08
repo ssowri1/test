c9c86d3d0677036a79979ee8fc2af03ad0ba7f20
Sowrirajan Sugumaran <ssowri1@gmail.com>
2019-03-08 18:38:37 +0530

    Add files via upload

---
+++ testMath/testMath/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testMath
+//
+//  Created by user on 08/03/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var A = [4, 2, 1]
+        var B = [2, 5, 3]
+        let F = 2
+        let output = CompanyEmployer.solution(&A, &B, F)
+//        print(output)
+        
+    }
+
+
+}
+
